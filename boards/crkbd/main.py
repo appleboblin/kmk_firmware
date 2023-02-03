c7cf65cb899ff342104e7549e27bc03a1e10e0d0
appleboblin <appleboblin2430@gmail.com>
2023-02-03 14:33:50 -0800

    Corne keymap
    Created keymap for corne and image for oled

--- boards/crkbd/main.py
+++ boards/crkbd/main.py
@@ -1,32 +1,64 @@
 from kb import KMKKeyboard
 
-from kmk.extensions.rgb import RGB
 from kmk.keys import KC
+
+from kmk.extensions.rgb import RGB
+from kmk.extensions.media_keys import MediaKeys
+from kmk.extensions.peg_oled_Display import Oled,OledDisplayMode,OledReactionType,OledData
+
 from kmk.modules.layers import Layers
-from kmk.modules.split import Split, SplitSide, SplitType
+from kmk.modules.split import Split, SplitSide
+from kmk.modules.tapdance import TapDance
+from kmk.modules.combos import Combos, Chord, Sequence
+
+from kmk.handlers.sequences import simple_key_sequence
 
 keyboard = KMKKeyboard()
 
-# Adding extensions
+# OLED
+oled_ext = Oled(OledData(image={0:OledReactionType.LAYER,1:["ma.bmp","co.bmp","mc.bmp","md.bmp","np.bmp"]}),toDisplay=OledDisplayMode.IMG,flip=False)
+keyboard.extensions.append(oled_ext) 
+
+# TapDance
+tapdance = TapDance()
+tapdance.tap_time = 300
+keyboard.modules.append(tapdance)
+
+# RGB
 rgb = RGB(pixel_pin=keyboard.rgb_pixel_pin, num_pixels=27, val_limit=100, hue_default=190, sat_default=100, val_default=5)
+keyboard.extensions.append(rgb)
 
-# TODO Comment one of these on each side
-split_side = SplitSide.LEFT
-split_side = SplitSide.RIGHT
-split = Split(split_type=SplitType.BLE, split_side=split_side)
+# TODO Split Rename drives https://github.com/KMKfw/kmk_firmware/blob/master/docs/en/split_keyboards.md
+split = Split()
+keyboard.modules.append(split)
 
-layers_ext = Layers()
+# Layers
+keyboard.modules.append(Layers())
 
-keyboard.modules = [layers_ext, split]
-keyboard.extensions = [rgb]
-#
-# Cleaner key names
+# Media Keys
+keyboard.extensions.append(MediaKeys())
+
+# Combo
+combos = Combos()
+keyboard.modules.append(combos)
+combos.combos = [
+    # match matrix so it doesn't matter layer
+    # [] keys
+    Chord((3, 4), KC.LBRACKET, match_coord=True)
+    Chord((28, 27), KC.RBRACKET, match_coord=True)
+
+    # Toggle Colemak
+    Chord((12, 13, 14, 15, 23), KC.RBRACKET, match_coord=True)
+    # Toggle Numpad
+    Chord((12, 13, 14, 23), KC.RBRACKET, match_coord=True)
+]
+
+# Key names
 _______ = KC.TRNS
 XXXXXXX = KC.NO
 
-LOWER = KC.MO(1)
-RAISE = KC.MO(2)
-ADJUST = KC.LT(3, KC.SPC)
+MISC = KC.LT(2, KC.ENTER, prefer_hold=True, tap_interrupted=False, tap_time=250)
+MEDIA = KC.TD(KC.LGUI, KC.MO(3))
 
 RGB_TOG = KC.RGB_TOG
 RGB_HUI = KC.RGB_HUI
@@ -36,31 +68,40 @@ RGB_SAD = KC.RGB_SAD
 RGB_VAI = KC.RGB_VAI
 RGB_VAD = KC.RGB_VAD
 
+# Macro/Sequence
+MNT = simple_key_sequence((KC.MEDIA_NEXT_TRACK, KC.MEDIA_FAST_FORWARD))
+MPT = simple_key_sequence((KC.MEDIA_PREV_TRACK, KC.MEDIA_REWIND))
 
 keyboard.keymap = [
-    [  #QWERTY
-        KC.TAB,    KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,                         KC.Y,    KC.U,    KC.I,    KC.O,   KC.P,  KC.BSPC,\
+    [  # QWERTY
+        KC.TAB,    KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,                         KC.Y,    KC.U,    KC.I,    KC.O,   KC.P,  KC.BSLS,\
         KC.LCTL,   KC.A,    KC.S,    KC.D,    KC.F,    KC.G,                         KC.H,    KC.J,    KC.K,    KC.L, KC.SCLN, KC.QUOT,\
         KC.LSFT,   KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,                         KC.N,    KC.M, KC.COMM,  KC.DOT, KC.SLSH, KC.RSFT,\
-                                            KC.LGUI,   LOWER,  ADJUST,     KC.ENT,   RAISE,  KC.RALT,
+                                            KC.LALT,  MEDIA,  KC.BSPC,      KC.SPC,  MISC, KC.RALT,
     ],
-    [  #LOWER
-        KC.ESC,   KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,                         KC.N6,   KC.N7,  KC.N8,   KC.N9,   KC.N0, KC.BSPC,\
-        KC.LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        KC.LEFT, KC.DOWN, KC.UP,   KC.RIGHT, XXXXXXX, XXXXXXX,\
-        KC.LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
-                                            KC.LGUI,   LOWER,  ADJUST,     KC.ENT,   RAISE,  KC.RALT,
+    [  # Colemak
+        KC.TAB,  _______, _______,    KC.F,    KC.P,    KC.G,                        KC.J,    KC.L,    KC.U,    KC.Y, KC.SCLN, KC.BSLS,\
+        KC.LCTL, _______,    KC.R,    KC.S,    KC.T,    KC.D,                     _______,    KC.N,    KC.E,    KC.I,    KC.O, KC.QUOT,\
+        KC.LSFT, _______, _______, _______, _______, _______,                        KC.K, _______, _______, _______, _______, KC.RSFT,\
+                                            _______, _______, _______,     _______, _______, _______,
     ],
-    [  #RAISE
-        KC.ESC, KC.EXLM,   KC.AT, KC.HASH,  KC.DLR, KC.PERC,                         KC.CIRC, KC.AMPR, KC.ASTR, KC.LPRN, KC.RPRN, KC.BSPC,\
-        KC.LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        KC.MINS,  KC.EQL, KC.LCBR, KC.RCBR, KC.PIPE,  KC.GRV,\
-        KC.LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        KC.UNDS, KC.PLUS, KC.LBRC, KC.RBRC, KC.BSLS, KC.TILD,\
-                                            KC.LGUI,   LOWER,  ADJUST,     KC.ENT,   RAISE,  KC.RALT,
+    [  # Misc
+        KC.ESC,    KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,                        KC.N6,   KC.N7,  KC.N8,    KC.N9,   KC.N0, _______,\
+        KC.CAPS, _______, _______, KC.MINS,  KC.EQL,  KC.GRV,                      KC.LEFT, KC.DOWN,  KC.UP, KC.RIGHT, _______, _______,\
+        KC.LSFT, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,\
+                                            _______, KC.LGUI, _______,     _______, _______, _______,
     ],
-    [  #ADJUST
-        RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
-        XXXXXXX, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
-                                            KC.LGUI,   LOWER,  ADJUST,     KC.ENT,   RAISE,  KC.RALT,
+    [  # Media
+        RGB_TOG,   KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,                         KC.F6,  KC.F7,   KC.F8,   KC.F9,  KC.F10, XXXXXXX,\
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        KC.F11, KC.F12, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC.MUTE,    MPT, KC.MPLY,     MNT, XXXXXXX, XXXXXXX,\
+                                            _______, _______, _______,     _______, KC.VOLD, KC.VOLU,
+    ],
+    [  # Numpad
+        RGB_TOG, XXXXXXX,   KC.N7,   KC.N8,   KC.N9, XXXXXXX,                        XXXXXXX, XXXXXXX,   KC.UP, XXXXXXX, XXXXXXX, XXXXXXX,\
+        XXXXXXX, XXXXXXX,   KC.N4,   KC.N5,   KC.N6, XXXXXXX,                        XXXXXXX, KC.LEFT, KC.DOWN, KC.RIGHT, XXXXXXX, XXXXXXX,\
+        XXXXXXX, XXXXXXX,   KC.N1,   KC.N2,   KC.N3,  KC.ENT,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+                                            KC.DOT,    KC.N0, _______,     _______,  _______, _______,
     ]
 ]
 
